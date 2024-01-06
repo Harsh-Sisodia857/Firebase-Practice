504302c655eed4cccf8c4030a2fb477ff587c4f7
Harsh-Sisodia857 <harsh2012007@akgec.ac.in>
2024-01-06 23:36:29 +0530

    User Authentication Service of Firebase to Sign Up User

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { app } from './firebase'
-import { getDatabase, set,ref } from 'firebase/database'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import './App.css'
 
 function App() {
   // creating instance which help us to interact with database
-  const db = getDatabase(app);
-  const sendData = () => {
-    set(ref(db, "users/harsh"), {
-      id: 1,
-      name : "Harsh",
-      course : "B.Tech"
+  const auth = getAuth(app);
+  const signUpUser = () => {
+    createUserWithEmailAndPassword(auth, "harsh@gmail.com", "123456").then((val) => {
+      console.log(val);
+      alert("Successfully Created User");
+    }).catch(err =>{
+      console.log(err);
     })
   }
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Firebase App Testing</h1>
-      <button onClick={sendData}>Send Data To Firebase</button>
+      <button onClick={signUpUser}>Sign Up User</button>
     </div>
-  )
+  );
 }
 
 export default App
