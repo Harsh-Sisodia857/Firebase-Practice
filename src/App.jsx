346256b3831e4bfd7b03fb5cacd0ae580c5af1cc
Harsh-Sisodia857 <harsh2012007@akgec.ac.in>
2024-01-06 23:42:20 +0530

    Login in the created user

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { app } from './firebase'
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import './App.css'
 
 function App() {
@@ -14,10 +18,21 @@ function App() {
       console.log(err);
     })
   }
+  const signInUser = () => {
+    signInWithEmailAndPassword(auth, "harsh@gmail.com", "123456")
+      .then((val) => {
+        console.log(val);
+        alert("Successfully Login User");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   return (
     <div className="App">
       <h1>Firebase App Testing</h1>
       <button onClick={signUpUser}>Sign Up User</button>
+      <button onClick={signInUser}>Sign In User</button>
     </div>
   );
 }
